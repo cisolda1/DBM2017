49c6ae36aa0191a5f5b2944ef6c303e05aee9f67
Christian Isolda <christian.isolda1@marist.edu>
2017-03-17 23:54:09 -0400

    Correct Lab 6

--- Lab 6/Lab 6.sql
+++ Lab 6/Lab 6.sql
@@ -1,48 +1,55 @@
 -- Christian Isolda
 -- Lab 6
 
--- 1:
+--1: Display	the	name	and	city	of	customers	who	live	in	any	city	that makes	the	most
+--different	kinds	of	products.	(There	are	two	cities	that	make	the	most	different	
+--products.	Return	the	name	and	city	of	customers	from	either	one	of	those.)
+
 SELECT name, city
 FROM customers
-WHERE city IN (SELECT city
-                FROM products
-                GROUP BY city
-                ORDER BY count(*) DESC
-                LIMIT 1);
+WHERE city IN(SELECT city FROM products GROUP BY city ORDER BY count(*) DESC LIMIT  1);
 
--- 2:
+--2: Display	the	names	of	products	whose	priceUSD	is	strictly	above	the	average	priceUSD,	
+--in	reverse-alphabetical	order.
+
 SELECT name
 FROM products
-WHERE priceUSD < ( SELECT AVG(priceUSD)
-                   FROM products)
-				ORDER BY name DESC ;
+WHERE priceUSD > (SELECT AVG(priceUSD) FROM products) 
+ORDER BY name Desc;
 
--- 3:
-select c.name, o.pid, o.totalUSD
-  from orders o inner join customers c on o.cid = c.cid
- order by totalUSD ASC;
+--3: Display	the	customer	name,	pid	ordered,	and	the	total	for	all	orders,	sorted	by	total	
+--from	low	to	high.
 
--- 4:
-SELECT c.name, COALESCE(sum(totalUSD), '0.00') AS sumTotalUSD
-FROM Customers c LEFT OUTER JOIN Orders o ON c.cid = o.cid
-GROUP BY o.cid, c.name
-ORDER BY c.name ASC;
+SELECT customers.name, orders.pid, orders.totalUSD
+FROM orders inner join customers on orders.cid = customers.cid
+ORDER BY totalUSD ASC;
 
--- 5:
-SELECT c.name AS "Customer", p.name AS "Product", a.name AS "Agent"
-FROM Customers c, Agents a, Products p, Orders o
-WHERE a.city = 'New York'
-  AND o.cid  = c.cid
-  AND o.aid  = a.aid
-  AND o.pid  = p.pid ;
+--4: Display	all	customer	names	(in	alphabetical	order)	and	their	total	ordered,	and	
+--nothing	more.	Use	coalesce	to	avoid	showing	NULLs.
 
--- 6:
-select q.totalUSD, correctBill.realBill
-  from orders q inner join (select o.ordnum, (o.qty * p.priceUSD) as realBill
-			      from orders o inner join products p on o.pid = p.pid) correctBill on correctBill.ordnum = q.ordnum 
-  where q.totalUSD != correctBill.realBill
-  
-  
+SELECT customers.name, COALESCE(sum(totalUSD)) AS sumUSD
+FROM customers LEFT OUTER JOIN orders ON customers.cid = orders.cid
+GROUP BY orders.cid, customers.name
+ORDER BY customers.name ASC;
+
+--5: Display	the	names	of	all	customers	who	bought	products	from	agents	based	in	
+--Newark	along	with	the	names	of	the	products	they	ordered,	and	the	names	of	the	
+--agents	who	sold	it	to	them.
+
+SELECT customers.name, products.name, agents.name
+FROM customers, products, agents, orders
+WHERE agents.city = 'Newark'
+AND orders.cid = customers.cid
+AND orders.aid = agents.aid
+AND orders.pid = products.pid;
+
+--6: Write	a	query	to	check	the	accuracy	of	the	totalUSD	column	in	the	Orders	table.	This	
+--means	calculating	Orders.totalUSD	from	data	in	other	tables	and	comparing	those	
+--values	to	the	values	in	Orders.totalUSD.	Display	all	rows	in	Orders	where	
+--Orders.totalUSD	is	incorrect,	if	any.
+SELECT orders.ordnumber, orders.totalUSD, products.priceusd
+FROM orders, products
+WHERE orders.totalUSD != ((products.priceusd)*(orders.qty));
 
 /* 7:     
       A LEFT OUTER JOIN will show all the rows from the table listed on the left side of the function. A RIGHT OUTER
